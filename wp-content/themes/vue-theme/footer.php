0e384c96f334c7e4dc47b279802152e8e52ff3b8
Matt Sobieray <matts@thefirmgraphics.com>
2017-04-28 18:13:54 -0600

    inital project commit

---
+++ wp-content/themes/vue-theme/footer.php
@@ -0,0 +1,6 @@
+		<footer class="footer">
+		</footer>
+		<script src="<?php bloginfo( 'template_url' ); ?>/dist/scripts/main.js"></script>
+		<?php wp_footer(); ?>
+	</body>
+</html>
---
+++ wp-content/themes/vue-theme/templates/template-home.php
@@ -0,0 +1,9 @@
+<?php /* Template Name: Home Template */ ?>
+
+<?php get_header(); ?>
+	<h1>Vue Theme</h1>
+	<div id="app">
+		{{ message }}
+	</div>
+
+<?php get_footer(); ?>
